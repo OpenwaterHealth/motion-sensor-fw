530baf7b6235893ac2c5cda87ae21d02abc6c9b7
ethan <ethan@openwater.cc>
2025-05-19 15:18:21 -0700

    change PID to keep away windows drivers issues on other machines

--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -65,7 +65,7 @@
 #define USBD_VID     0x483
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_HS     0x5A5A
+#define USBD_PID_HS     0x5A5C
 #define USBD_PRODUCT_STRING_HS     "OpenMotion Sensor Module"
 #define USBD_CONFIGURATION_STRING_HS     "CDC Config"
 #define USBD_INTERFACE_STRING_HS     "CDC Interface"
