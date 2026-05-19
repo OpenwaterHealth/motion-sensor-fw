5f1d6985aed4bef510f13db92b30841c2d96d99c
George Vigelette <george@openwater.cc>
2026-05-19 00:08:59 -0400

    aded long watchdog need to fix this

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -401,6 +401,7 @@ int main(void)
 
   comms_host_start();
   // fill_frame_buffers();
+  HAL_IWDG_Refresh(&hiwdg1);
   printf("System Running\r\n");
   /* USER CODE END 2 */
 
@@ -415,6 +416,7 @@ int main(void)
     check_streaming();
     poll_mcu_temperature();  /* Print warning if MCU junction temp above threshold */
     USB_RecoveryCheck();     /* EFT/lock-up watchdog: rebuild USB stack if bus goes dead */
+    HAL_IWDG_Refresh(&hiwdg1);
   }
 
   /* USER CODE END 3 */
@@ -602,9 +604,9 @@ static void MX_IWDG1_Init(void)
 
   /* USER CODE END IWDG1_Init 1 */
   hiwdg1.Instance = IWDG1;
-  hiwdg1.Init.Prescaler = IWDG_PRESCALER_32;
+  hiwdg1.Init.Prescaler = IWDG_PRESCALER_128;
   hiwdg1.Init.Window = 4095;
-  hiwdg1.Init.Reload = 1000-1;
+  hiwdg1.Init.Reload = 4095;
   if (HAL_IWDG_Init(&hiwdg1) != HAL_OK)
   {
     Error_Handler();
